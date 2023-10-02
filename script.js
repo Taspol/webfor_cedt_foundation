abc1528a737cf747bcd6220455095324022327fe
Taspol <taspolsd@gmail.com>
2023-10-02 13:30:28 +0700

    updateloginpage

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+
+
 function openForm() {
   document.getElementById("myForm").style.display = "block";
-}
+  }
 
-function closeForm() {
+  function closeForm() {
   document.getElementById("myForm").style.display = "none";
-}
+  }
